d43a3e17d3f44641d66beaf0d6b6f6ffb1fbbfd3
EnriqueTM04 <enriquetabo03@gmail.com>
2024-06-05 23:51:43 -0600

    Inicio
    Comenzando proyecto

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nombre de la tienda</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+
+    <header data-bs-theme="dark">
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <strong>Store</strong>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>  
+
+                <div class="collapse navbar-collapse" id="navbarHeader">
+                <!--mb-2 mb-lg-0-->
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active">Productos</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Catalogo</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Contacto</a>
+                        </li>
+                    </ul>
+
+                    <a href="carrito.php" class="btn btn-primary">Carrito</a>
+                </div>
+
+            </div>
+        </div>
+    </header>
+
+    <main class="container productos">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="/images/productos/1/1.png" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Iphone</h5>
+                        <p class="card-text">$ 700</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="#" class="btn btn-primary">Ver Más</a>
+                            </div>
+                            <small class="btn btn-success">9 mins</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>      
+    </main>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
